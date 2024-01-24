66ab107e1502abd0e8f6b6a1282351abf6d1c5c3
Sianglife <sianglifen@gmail.com>
2024-01-24 15:17:41 +0800

    add get Problem list

---
+++ src/routes/Problems.ts
@@ -0,0 +1,19 @@
+import { IReq, IRes } from './types/express/misc';
+import { ISubmitRequest, ISubmission } from '@src/models/Submission';
+import axios from 'axios';
+import EnvVars from '@src/constants/EnvVars';
+import { MongoClient } from 'mongodb';
+
+const mongoURI = EnvVars.DB.URI;
+const mongo = new MongoClient(mongoURI);
+
+function getProblemList(req: IReq, res: IRes) {
+    mongo.db('Judge').collection('Problems').find({}, { projection: { _id: 0  } }).toArray()
+        .then((data) => {
+            return res.json(data);
+        })
+}
+
+export default {
+    getProblemList
+} as const;
